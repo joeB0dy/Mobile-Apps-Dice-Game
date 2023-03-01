51249ce0797ba3f68821455dfd37b983ef603d4f
Jose <sjose@umich.edu>
2023-03-01 15:29:16 -0500

    Working on Control flow in Kotlin now

--- app/src/main/java/com/example/dicepractice/MainActivity.kt
+++ app/src/main/java/com/example/dicepractice/MainActivity.kt
@@ -14,7 +14,10 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
-
+//the actual coding
+        var points = 1 //re-initialized to dice value
+        var player1Points = 0
+        var player2Points = 0
         //set button event handler. below is exclusive of top value.
         binding.btnRollDie.setOnClickListener {
             var diceValue = Random.nextInt(1,7)  //use kotlin version of random. (Alt + Enter)
@@ -24,7 +27,68 @@ class MainActivity : AppCompatActivity() {
             var resourceID = resources.getIdentifier(imageName, "drawable", getPackageName())
 
             binding.ivDie.setImageResource(resourceID)
+
+            var num1 = Random.nextInt(0,100)
+            var num2 = Random.nextInt(0,100)
+            var answer = 0
+
+            var points = diceValue //re-initialized to dice value
+
+            //Conditional Switch Statement in Kotlin
+            when (diceValue){
+                1->{    //addition
+                    answer = num1 + num2
+
+                    val builder = StringBuilder()       //complicated manner to concatenate values to output
+                    builder.append(num1)
+                    builder.append(" + ")
+                    builder.append(num2)
+                    builder.append(" = ")
+                    binding.tvMath.text = builder.toString()
+                }
+                2->{    //subtraction
+
+                    answer = num1-num2
+                    val builder = StringBuilder()
+                    builder.append(num1)
+                    builder.append(" - ")
+                    builder.append(num2)
+                    builder.append(" = ")
+                    binding.tvMath.text = builder.toString()
+                }
+                3->{    //multiplication
+                    answer = num1*num2
+                    val builder = StringBuilder()
+                    builder.append(num1)
+                    builder.append(" * ")
+                    builder.append(num2)
+                    builder.append(" = ")
+                    binding.tvMath.text = builder.toString()
+                }
+                4->{    //double points roll again
+                    points *=2
+                }
+                5->{    //lose a turn
+
+                }
+                6->  {  //JACKPOT
+                    points *=5
+                }
+            }
+
+
         }//end. test on macbook with virtual AVD
+
+
+
+
+        //guess button stuff
+        binding.btnGuess.setOnClickListener{
+
+        }
+
+
+
     }//on Create end
 }
 //step one, set viewbinding to true in build.gradle
